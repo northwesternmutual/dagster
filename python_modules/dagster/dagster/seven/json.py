45e1bdf7fd1697495f316ce64a073563611cf365
prha <prha@elementl.com>
2021-12-06 16:58:50 -0800

    orjson

--- python_modules/dagster/dagster/seven/json.py
+++ python_modules/dagster/dagster/seven/json.py
@@ -1,9 +1,12 @@
 # pylint: disable=unused-import
 from functools import partial
+
 from json import dump as dump_
-from json import dumps as dumps_
+from orjson import dumps as dumps_
+
 from json import load as load_
-from json import loads as loads_
+from orjson import loads
+from orjson import OPT_SORT_KEYS
 
 try:
     from json import JSONDecodeError
@@ -12,8 +15,9 @@ except ImportError:
 
 dump = partial(dump_, sort_keys=True)
 
-dumps = partial(dumps_, sort_keys=True)
+
+def dumps(*args, **kwargs):
+    return partial(dumps_, option=OPT_SORT_KEYS)(*args, **kwargs).decode("utf-8")
+
 
 load = partial(load_, strict=False)
-
-loads = partial(loads_, strict=False)
